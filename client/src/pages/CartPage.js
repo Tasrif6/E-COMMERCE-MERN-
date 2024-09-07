f7872dbc9bd8760355c1df87317fabbbc7ba2d1e
AbKadir11 <md.abdul.kadir@g.bracu.ac.bd>
2024-09-08 02:19:42 +0600

    Create CartPage.js

---
+++ client/src/pages/CartPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from "./../components/Layout/Layout";
+
+const CartPage = () => {
+  return (
+    <layout>
+      <div className= "container">
+        <div className= "row">
+          <h1> Your Cart </h1> 
+        </div>
+      </div>
+    </layout>
+  )
+}
+
+export default CartPage
