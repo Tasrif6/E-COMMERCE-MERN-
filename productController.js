c211687a4fe4dcf762c8a6ec45530a8f5c43ae6e
Maisha-mini <maisha.binta.alam@g.bracu.ac.bd>
2024-09-09 01:13:20 +0600

    Add files via upload

---
+++ productController.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+//search product
+export const searchProductController = async (req,res) => {
+    try {
+
+        const {keyword} = req.params
+        const result = await productModel
+            .find({
+                $or: [
+                    {name:{$regex :keyword, $options:"i" } },
+                    {description:{$regex :keyword, $options:"i" } },
+                ],
+            })
+            .select('-photo');
+        res.json(result);
+    }   catch (error) {
+        console.log(error);
+        res.status(400).send({
+            success:false,
+            message:'Error In Search Product API',
+            error 
+      });
+    }
+};
