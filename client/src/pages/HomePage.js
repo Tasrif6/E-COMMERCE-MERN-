2896e78090cb53cd2f48257172c4c90eb4c31ae1
AbKadir11 <md.abdul.kadir@g.bracu.ac.bd>
2024-09-11 23:33:25 +0600

    Update HomePage.js

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -16,132 +16,334 @@
 // export default HomePage
 
 
-import React, { useState, useEffect } from 'react;
+// import React, { useState, useEffect } from 'react;
+// import Layout from "../components/Layout/Layout";
+// import { useNavigate } from 'react-router-dom';
+// import axios from 'axios';
+// import { Checkbox, Radio } from 'antd';
+// import { Prices } from '../components/Prices';
+
+
+// const HomePage = () => {
+//   const navigate = useNavigate();
+//   const [ cart, setCart ] = useCart();
+//   const [ products, setProducts ] = useState([]);
+//   const [ categories, setCategories ] = useState([]);
+//   const [ checked setChecked ] = useState([]);
+//   const [ radio, setRadio ] = useState([]);
+//   const [ total, setTotal ] = useState([]);
+//   const [ page, setPage ] = useState([]);
+//   const [ loading, setLoading ] = useState([]);
+
+//   const getAllCategory = async () => {
+//     try {
+//       const { data } = await.axios.get('api/v1/category/get-category');
+//       if (data?.success) {
+//         setCategories(data?.category);
+//       }
+//     } catch(error) {
+//       console.log(error);
+//     }
+//   }; 
+
+//   useEffect(() => {
+//     getAllCategory();
+//     getTotal();
+//   }, []);
+
+//   const getAllProducts = async () => {
+//     try {
+//       setLoading(true);
+//       const { data } = await.axios.get('api/v1/product/product-list/${page}');
+//       setLoading(false);
+//       setProducts(data.products);
+//     } catch(error) {
+//       setLoading(false);
+//       console.log(error);
+//     }
+//   };
+
+//   // get total part
+
+//   const getTotal = async () => {
+//     try {
+//       const { data } = await.axios.get('api/v1/product/product-count');
+//       setTotal(data?.total);
+//     } catch(error) {
+//       console.log(error);
+//     }
+//   };
+
+//   useEffect(() => {
+//     if (page === 1) return;
+//     loadMore();
+//   }, [page]);
+
+
+//   const loadMore = async () => {
+//     try {
+//       setLoading(true);
+//       const { data } = await.axios.get('api/v1/product/product-list/${page}');
+//       setLoading(false);
+//       setProducts([...products,...data?.products]);
+//     } catch(error) {
+//       console.log(error);
+//       setLoading(false);
+//     }
+//   };
+
+
+
+//   const handleFilter = (value, id) => {
+//     let all = [..checked];
+//     if (value) {
+//       all.push(id);
+//     } else {
+//         all = all.filter((c) => c !== id);
+//     }
+//     setChecked(all);
+//   };
+
+//   useEffect(() => {
+//     if (!checked.length || !radio.length) getAllProducts();
+//   }, [checked.length, radio.length]);
+
+//   useEffect(() => {
+//     if (checked.length || radio.length) filterProduct();
+//   }, [checked, radio]);
+
+
+//   const filterProduct = async () => {
+//     try {
+//       const { data } = await.axios.get('api/v1/product/product-filters', {
+//          checked,
+//          radio,
+//       });
+//       setProducts(data?.products);
+//     } catch(error) {
+//       console.log(error);
+//     }
+//   }; 
+
+//   return (
+//     <layout title = {"All Products- Best Offers"}>
+//       <div className = "container-fluid row mt-3">
+//         <div className= "col-md-2">
+//           <h4 className= "text-center"> Filter By Category </h4>
+//           <div className= "d-flex flex-column">
+//             {
+
+
+
+
+      
+//   };
+      
+      
+  
+
+
+import React, { useState, useEffect } from 'react';
 import Layout from "../components/Layout/Layout";
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { Checkbox, Radio } from 'antd';
 import { Prices } from '../components/Prices';
+import { useCart } from '../context/cart';
+import toast from 'react-hot-toast';
+
 
 
 const HomePage = () => {
-  const navigate = useNavigate();
-  const [ cart, setCart ] = useCart();
-  const [ products, setProducts ] = useState([]);
-  const [ categories, setCategories ] = useState([]);
-  const [ checked setChecked ] = useState([]);
-  const [ radio, setRadio ] = useState([]);
-  const [ total, setTotal ] = useState([]);
-  const [ page, setPage ] = useState([]);
-  const [ loading, setLoading ] = useState([]);
+    const navigate = useNavigate();
+    const [ cart, setCart ] = useCart();
+    const [ products, setProducts ] = useState([]);
+    const [ categories, setCategories ] = useState([]);
+    const [ checked, setChecked ] = useState([]);
+    const [ radio, setRadio ] = useState([]);
+    const [ total, setTotal ] = useState([]);
+    const [ page, setPage ] = useState([]);
+    const [ loading, setLoading ] = useState([]);
 
-  const getAllCategory = async () => {
-    try {
-      const { data } = await.axios.get('api/v1/category/get-category');
-      if (data?.success) {
-        setCategories(data?.category);
-      }
-    } catch(error) {
-      console.log(error);
-    }
-  }; 
+    //get cat
+    const getAllCategory = async () => {
+        try {
+            const { data } = await axios.get('api/v1/category/get-category');
+            if (data?.success) {
+            setCategories(data?.category);
+            }
+        } catch(error) {
+            console.log(error);
+        }
+    }; 
 
-  useEffect(() => {
-    getAllCategory();
-    getTotal();
-  }, []);
+    useEffect(() => {
+        getAllCategory();
+        getTotal();
+    }, []);
 
-  const getAllProducts = async () => {
-    try {
-      setLoading(true);
-      const { data } = await.axios.get('api/v1/product/product-list/${page}');
-      setLoading(false);
-      setProducts(data.products);
-    } catch(error) {
-      setLoading(false);
-      console.log(error);
-    }
-  };
+    const getAllProducts = async () => {
+        try {
+            setLoading(true);
+            const { data } = await axios.get(`api/v1/product/product-list/${page}`);
+            setLoading(false);
+            setProducts(data.products);
+        } catch(error) {
+            setLoading(false);
+            console.log(error);
+        }
+    };
 
-  // get total part
+    // get total part
 
-  const getTotal = async () => {
-    try {
-      const { data } = await.axios.get('api/v1/product/product-count');
-      setTotal(data?.total);
-    } catch(error) {
-      console.log(error);
-    }
-  };
+    const getTotal = async () => {
+        try {
+            const { data } = await axios.get('api/v1/product/product-count');
+            setTotal(data?.total);
+        } catch(error) {
+            console.log(error);
+        }
+    };
 
-  useEffect(() => {
-    if (page === 1) return;
-    loadMore();
-  }, [page]);
-
-
-  const loadMore = async () => {
-    try {
-      setLoading(true);
-      const { data } = await.axios.get('api/v1/product/product-list/${page}');
-      setLoading(false);
-      setProducts([...products,...data?.products]);
-    } catch(error) {
-      console.log(error);
-      setLoading(false);
-    }
-  };
+    useEffect(() => {
+        if (page === 1) return;
+        loadMore();
+    }, [page]);
 
 
-
-  const handleFilter = (value, id) => {
-    let all = [..checked];
-    if (value) {
-      all.push(id);
-    } else {
-        all = all.filter((c) => c !== id);
-    }
-    setChecked(all);
-  };
-
-  useEffect(() => {
-    if (!checked.length || !radio.length) getAllProducts();
-  }, [checked.length, radio.length]);
-
-  useEffect(() => {
-    if (checked.length || radio.length) filterProduct();
-  }, [checked, radio]);
-
-
-  const filterProduct = async () => {
-    try {
-      const { data } = await.axios.get('api/v1/product/product-filters', {
-         checked,
-         radio,
-      });
-      setProducts(data?.products);
-    } catch(error) {
-      console.log(error);
-    }
-  }; 
-
-  return (
-    <layout title = {"All Products- Best Offers"}>
-      <div className = "container-fluid row mt-3">
-        <div className= "col-md-2">
-          <h4 className= "text-center"> Filter By Category </h4>
-          <div className= "d-flex flex-column">
-            {
+    const loadMore = async () => {
+        try {
+            setLoading(true);
+            const { data } = await axios.get(`api/v1/product/product-list/${page}`);
+            setLoading(false);
+            setProducts([...products,...data?.products]);
+        } catch(error) {
+            console.log(error);
+            setLoading(false);
+        }
+    };
 
 
 
+    const handleFilter = (value, id) => {
+        let all = [...checked];
+        if (value) {
+            all.push(id);
+        } else {
+            all = all.filter((c) => c !== id);
+        }
+        setChecked(all);
+    };
 
-      
-  };
-      
-      
-  
-    
-  
+    useEffect(() => {
+        if (!checked.length || !radio.length) getAllProducts();
+    }, [checked.length, radio.length]);
+
+    useEffect(() => {
+        if (checked.length || radio.length) filterProduct();
+    }, [checked, radio]);
+
+
+
+    //get filter product
+    const filterProduct = async () => {
+        try {
+            const { data } = await axios.post('api/v1/product/product-filters', {
+                checked,
+                radio,
+            });
+            setProducts(data?.products);
+        } catch(error) {
+            console.log(error);
+        }
+    };
+
+
+    return (
+        <Layout title = {"All Products - Best Offers"}>
+            <div className = "container-fluid row mt-3">
+                <div className = "col-md-2">
+                    <h4 className = "text-center">Filter By Category</h4>
+                    <div className = "d-flex flex-column">
+                        {categories?.map((c) => (
+                            <Checkbox key = {c._id} onChange= {(e) => handleFilter(e.target.checked, c._id)} >
+                                {c.name}
+                            </Checkbox>
+                        ))}
+                    </div>
+                
+                    {/* <div className = "d-flex flex-column">
+                    {categories?.map((c) => (
+                        <Checkbox
+                        key = {c._id}
+                        onChange= {(e) => handleFilter(e.target.checked, c._id)}
+                        >
+                        {c.name}
+                        </Checkbox>
+                    ))}
+                    </div> */}
+                    {/* Price Filter */}
+                    <h4 className = "text-center">Filter By Price</h4>
+                    <div className = "d-flex flex-column">
+                        <Radio.Group onChange= {(p) => setRadio(e.target.value)}>
+                            {Prices?.map((c) => ( 
+                            <div key = {p._id}>
+                                <Radio value = {p.array} > {p.name}
+                                </Radio>
+                            </div>
+                            ))}
+                        </Radio.Group>
+                    </div>
+                </div>
+                <div className='col-md-9'>
+                    {JSON.stringify(radio, null, 4)}
+                    <h4 className = "text-center">All Product List</h4>
+                    <div className = "d-flex flex-wrap"></div>
+                        {products?.map((p) => (
+                            <div className='card m-2' style={{width: '18rem'}}>
+                                <img src= {`/api/v1/product/product-photo/${p._id}`}
+                                className='card-img-top'
+                                alt = {p.name}/>
+
+                                <div className='card-body'>
+                                    <h5 className='card-title'>{p.name}</h5>
+                                    <p className='card-text'>{p.description.subString(0,30)}...</p>
+                                    <p className='card-text'>{p.price} bdt </p>
+                                    <button className= "btn btn-primary ms-1"
+                                        onClick={() => navigate(`/product/${p.slug}`)
+                                    }>More Details
+                                    </button>
+                                    <button class= "btn btn-secondary ms-1" 
+                                    onClick={() => {setCart(...cart, p)
+                                        toast.success("Item added to the cart")
+                                    }}> Add to Cart
+                                    </button>
+                                </div>
+                            </div>
+                        ))}
+                        <h1>Products</h1>
+                    </div>
+                </div>
+        </Layout>
+    )
+
+
+
+
+
+
+};
+
+
+
+
+        
+        
+
+
+
+
+
+
   
 
