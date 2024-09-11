b4239354dece5c338bbeda97828443d7c7178270
AbKadir11 <md.abdul.kadir@g.bracu.ac.bd>
2024-09-11 23:32:02 +0600

    Update CartPage.js

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import Layout from "./../components/Layout/Layout";
+import { useCart } from "../context/cart";
+import { useNavigate } from "react-router-dom";
 
 const CartPage = () => {
+    const [auth, setAuth] = useAuth()
+    const [cart, setCart] = useCart()
+    const navigate = useNavigate()
   return (
     <layout>
       <div className= "container">
         <div className= "row">
-          <h1> Your Cart </h1> 
+          <div className= "col-md-12">
+            <h1 className= "text-center bg-light p-2"> 
+              {`Hello ${auth?.token && auth?.user?.name}`}
+            </h1> 
+            <h4 className = "text-center">
+              {cart?. length > 1 ? `You have ${cart.length} items in your cart ${auth?.token} ? "" : "Please Login"
         </div>
       </div>
     </layout>
