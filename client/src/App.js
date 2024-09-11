971edd6a73327dd59293a45e27c3a5c9ea1a03a3
Maisha-mini <maisha.binta.alam@g.bracu.ac.bd>
2024-09-11 08:49:32 +0600

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -5,19 +5,19 @@ import Contact from './pages/Contact';
 import Policy from './pages/Policy';
 import Pagenotfound from './pages/Pagenotfound';
 import Register from './pages/Auth/Register';
-import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Login from './pages/Auth/Login';
 import PrivateRoute from './components/Routes/Private';
 import Dashboard from './pages/user/Dashboard';
 import ForgotPassword from './pages/Auth/ForgotPassword';
-
+import Search from "./pages/Search";
 
 function App() {
   return (
     <>
     <Routes>
        <Route path='/' element={<HomePage />}/>
+       <Route path='/search' element={<Search />}/>
        <Route path="/dashboard" element={<PrivateRoute />}>
           <Route path="" element={<Dashboard />} />
        </Route>
