3f6eeac5efc7076f9b26c04b6f66ec75d9132dfb
AbKadir11 <md.abdul.kadir@g.bracu.ac.bd>
2024-09-11 08:08:21 +0600

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,4 +1,28 @@
-import router from
+import express from "express"
+import {
+  createProductController,
+  deleteProductController,
+  getProductController,
+  getSingleProductController,
+  productFiltersController,
+  productPhotoController,
+  updateProductController,
+} from '/..controllers/product-controller.js';
+import { isAdmin, requiresSignIn } from '../middlewares/authMiddleware.js';
+import formidable from "express-formidable"
+
+const router = express.router();
+
+router.post(
+  requiresSignIn,
+  isAdmin
+  formidable(),
+  createProductController,
+);
+
+router.put();
+
+
 
 router.get('/product-filters', productsFilterController)
 
