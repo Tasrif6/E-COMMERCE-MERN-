8c62f7f732e0102a9f1389463f9571de030413f5
AbKadir11 <md.abdul.kadir@g.bracu.ac.bd>
2024-09-12 02:04:31 +0600

    Update CartPage.js

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -4,9 +4,21 @@ import { useCart } from "../context/cart";
 import { useNavigate } from "react-router-dom";
 
 const CartPage = () => {
-    const [auth, setAuth] = useAuth()
+    const [auth, setAuth] = useAuth() 
     const [cart, setCart] = useCart()
     const navigate = useNavigate()
+
+
+    const removeCartItem = (pid) => {
+        try{
+            let myCart = [...cart];
+            let myIndex = myCart.findIndex((item) => item._id === pid);
+            myCart.splice(index, 1);
+            setCart(myCart);
+            
+        }   catch (error) {
+            console.log(error);
+        }
   return (
     <layout>
       <div className= "container">
@@ -16,8 +28,41 @@ const CartPage = () => {
               {`Hello ${auth?.token && auth?.user?.name}`}
             </h1> 
             <h4 className = "text-center">
-              {cart?. length > 1 ? `You have ${cart.length} items in your cart ${auth?.token} ? "" : "Please Login"
+              {cart?. length > 1 
+                ? `You have ${cart.length} items in your cart ${
+                    auth?.token ? "" : "Please Login to check"
+                  }`
+                : "Your cart is empty"}
+            </h4>
         </div>
+      </div>
+      <div className= "row">
+          <div className= "col-md-8">
+            {
+                cart?.map(p => (
+                    <div className= "row mb-2 p-3 card flex-row">
+                      <div className= "col-md-4">
+                        <img 
+                            src= {`/api/v1/product/product-photo/${p._id}`}
+                            className='card-img-top'
+                            alt = {p.name}
+                            width = "100px"
+                            height = {"100px"}
+                        />
+                      </div>
+                      <div className= "col-md-8">
+                        <p>{p.name}</p>
+                        <p>{p.description.subString(0,30)}...</p>
+                        <p>Price: {p.price} bdt </p>
+                        <button className= "btn btn-danger" onClick={() => removeCartItem(p._id)} > Remove </button>
+                      </div>
+                    </div>
+                ))
+            } 
+          </div>
+          <div className= "col-md-4">
+                Checkout | Payment
+          </div>
       </div>
     </layout>
   )
