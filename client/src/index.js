2ca3be1736353dcd4915c6ea111375149ec80644
Maisha-mini <maisha.binta.alam@g.bracu.ac.bd>
2024-09-11 22:27:56 +0600

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -9,11 +9,13 @@ import {AuthProvider} from "./context/auth";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <AuthProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <SearchProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </SearchProvider>
   </AuthProvider>
-);
+); 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
