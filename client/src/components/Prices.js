31fd77c34d47e53448211945c71c355dec233c03
AbKadir11 <md.abdul.kadir@g.bracu.ac.bd>
2024-09-19 06:27:54 +0600

    Create Prices.js

---
+++ client/src/components/Prices.js
@@ -0,0 +1,32 @@
+export const Prices =[
+    {
+        _id:0,
+        name:"0 to 200 tk",
+        array:[0,200]
+    },
+    {
+        _id:1,
+        name:"201 to 400 tk",
+        array:[201,400]
+    },
+    {
+        _id:2,
+        name:"401 to 600 tk",
+        array:[401,600]
+    },
+    {
+        _id:3,
+        name:"601 to 800 tk",
+        array:[601,800]
+    },
+    {
+        _id:4,
+        name:"801 to 1000 tk",
+        array:[801,1000]
+    },
+    {
+        _id:5,
+        name:"1001 or more",
+        array:[1001,9000]
+    },
+];
