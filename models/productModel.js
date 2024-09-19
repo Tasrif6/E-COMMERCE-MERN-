f1783800ce91744328490d5c141202b0de698f0c
AbKadir11 <md.abdul.kadir@g.bracu.ac.bd>
2024-09-19 07:05:35 +0600

    Create productModel.js

---
+++ models/productModel.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required: true
+    },
+    slug:{
+        type:String,
+        required: true
+    },
+    description:{
+        type:String,
+        required: true
+    },
+    price:{
+        type:Number,
+        required: true
+    },
+    category:{
+        type:mongoose.objectID,
+        ref: 'category',
+        required: true
+    }, quantity:{
+        type:Number,
+        required: true
+    },
+    photo:{
+        type: ArrayBuffer,
+        contentType:String,
+    },
+    shipping:{
+        type: Boolean
+    },
+}, {timeStamps: true} );
+
+export default mongoose.model('products', productSchema)
