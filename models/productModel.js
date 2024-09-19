1e8b443ba8c68b51be9d15b3ca4dc0d4228d5d27
AbKadir11 <md.abdul.kadir@g.bracu.ac.bd>
2024-09-19 09:02:34 +0600

    Update productModel.js

--- models/productModel.js
+++ models/productModel.js
@@ -26,7 +26,7 @@ const productSchema = new mongoose.Schema({
         required: true
     },
     photo:{
-        type: ArrayBuffer,
+        data: ArrayBuffer,
         contentType:String,
     },
     shipping:{
